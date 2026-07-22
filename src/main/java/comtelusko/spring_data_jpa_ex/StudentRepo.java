51a8ba0bb6d1e542cd6302c4d85f43848e1906dd
Bhupendra <chouhanbhupi0104@gmail.com>
2026-07-22 21:58:18 +0530

    Save data in database student successfully

---
+++ src/main/java/comtelusko/spring_data_jpa_ex/StudentRepo.java
@@ -0,0 +1,10 @@
+package comtelusko.spring_data_jpa_ex;
+
+import comtelusko.spring_data_jpa_ex.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepo extends JpaRepository <Student, Integer>{
+
+}
